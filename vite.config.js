19081086769fc4cf3c16a1a4639af847413e768e
floresz <floresz@globalhitss.com>
2023-03-10 00:16:30 -0600

    base updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://ZeuFlores.github.io/prueba-git'
+  base: 'https://zeuflowers.github.io/prueba-git'
 })
